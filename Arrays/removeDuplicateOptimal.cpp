007e4678c3caa128b8d9e25169aa33b420f657da
Abay Kapatia <abaykapatia@gmail.com>
2025-12-23 18:23:33 +0530

    Update removeDuplicateOptimal.cpp

--- Arrays/removeDuplicateOptimal.cpp
+++ Arrays/removeDuplicateOptimal.cpp
@@ -1,3 +1,10 @@
+//remove duplicate using 2 pointer approach 
+//[1 1 2 3 3 4]
+///j i
+///j   i        ---> j!=i so assign it value of i
+///[1 2 2 3 3 4]
+///[  j i       ---> new positions
+
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
